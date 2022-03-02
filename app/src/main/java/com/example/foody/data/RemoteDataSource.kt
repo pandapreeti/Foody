6a23b8c73b62bbc138114c60525538a26171188c
Preeti Panda <preeti.panda@clover.com>
2022-03-01 16:31:48 -0800

    Initial commit

---
+++ app/src/main/java/com/example/foody/data/RemoteDataSource.kt
@@ -0,0 +1,16 @@
+package com.example.foody.data
+
+import com.example.foody.data.network.FoodRecipeApi
+import com.example.foody.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipeApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
+
+}
---
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.foody.util
+
+class Constants {
+
+    companion object{
+        const val BASE_URL = "https://api.sponacular.com"
+        const val API_KEY = "0d3c33eaacb64664a81b9e4978be6b13"
+
+    }
+}
