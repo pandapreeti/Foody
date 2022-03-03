ebb5aeff2371f324659bd1f78f081c4e1cff2e62
Preeti Panda <preeti.panda@clover.com>
2022-03-03 10:20:56 -0800

    Add recipe images

--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -3,8 +3,8 @@ package com.example.foody.util
 class Constants {
 
     companion object{
-        const val BASE_URL = "https://api.sponacular.com"
-        const val API_KEY = "0d3c33eaacb64664a81b9e4978be6b13"
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "4e85d97dddf443d3b98f92a24a4ae832"
 
     }
 }
